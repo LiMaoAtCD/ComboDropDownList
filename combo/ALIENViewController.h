51ac68cdd98effe05eb2d5ecbcd97eca8b3bf0f3
alienLi <liamscofield@icloud.com>
2014-04-25 08:59:04 +0800

    first commit

--- combo/ALIENViewController.h
+++ combo/ALIENViewController.h
@@ -7,7 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "UITableView+DataSourceBlocks.h"
+@class TableViewWithBlock;
 
 @interface ALIENViewController : UIViewController
+{
+    BOOL isOpened;
+}
+
+@property (strong, nonatomic) IBOutlet UIButton *openButton;
+@property (strong, nonatomic) IBOutlet UITextField *inputTextField;
+@property (strong, nonatomic) IBOutlet TableViewWithBlock *tb;
+- (IBAction)changeOpenStatus:(id)sender;
+
 
 @end
--- combo/ALIENViewController.m
+++ combo/ALIENViewController.m
@@ -7,7 +7,10 @@
 //
 
 #import "ALIENViewController.h"
-
+#import <QuartzCore/QuartzCore.h>
+#import "SelectionCell.h"
+#import "UITableView+DataSourceBlocks.h"
+#import "TableViewWithBlock.h"
 @interface ALIENViewController ()
 
 @end
@@ -18,6 +21,27 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    isOpened=NO;
+    [_tb initTableViewDataSourceAndDelegate:^(UITableView *tableView,NSInteger section){
+        return (NSUInteger)5;
+        
+    } setCellForIndexPathBlock:^(UITableView *tableView,NSIndexPath *indexPath){
+        SelectionCell *cell=[tableView dequeueReusableCellWithIdentifier:@"SelectionCell"];
+        if (!cell) {
+            cell=[[[NSBundle mainBundle]loadNibNamed:@"SelectionCell" owner:self options:nil]objectAtIndex:0];
+            [cell setSelectionStyle:UITableViewCellSelectionStyleGray];
+        }
+        [cell.lb setText:[NSString stringWithFormat:@"Select %ld",(long)indexPath.row]];
+        return cell;
+    } setDidSelectRowBlock:^(UITableView *tableView,NSIndexPath *indexPath){
+        SelectionCell *cell=(SelectionCell*)[tableView cellForRowAtIndexPath:indexPath];
+        _inputTextField.text=cell.lb.text;
+        [_openButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+    }];
+    
+    [_tb.layer setBorderColor:[UIColor lightGrayColor].CGColor];
+    [_tb.layer setBorderWidth:2];
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +49,42 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (IBAction)changeOpenStatus:(id)sender {
+    
+    if (isOpened) {
+        
+        
+        
+        [UIView animateWithDuration:0.3 animations:^{
+            UIImage *closeImage=[UIImage imageNamed:@"dropdown.png"];
+            [_openButton setImage:closeImage forState:UIControlStateNormal];
+            
+            CGRect frame=_tb.frame;
+            
+            frame.size.height=1;
+            [_tb setFrame:frame];
+            
+        } completion:^(BOOL finished){
+            
+            isOpened=NO;
+        }];
+    }else{
+        
+        
+        [UIView animateWithDuration:0.3 animations:^{
+            UIImage *openImage=[UIImage imageNamed:@"dropup.png"];
+            [_openButton setImage:openImage forState:UIControlStateNormal];
+            
+            CGRect frame=_tb.frame;
+            
+            frame.size.height=200;
+            [_tb setFrame:frame];
+        } completion:^(BOOL finished){
+            
+            isOpened=YES;
+        }];
+        
+        
+    }
+}
 @end
